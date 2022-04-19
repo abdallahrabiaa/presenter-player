5383d0f30d414d5f43b81560ca37060b887d104a
abdullhRabea <abdallahrabiaa914_sd@fcis.bsu.edu.eg>
2022-04-19 03:34:36 +0200

    try solve refresh bug

--- presentation/js/handle.js
+++ presentation/js/handle.js
@@ -1,7 +1,6 @@
 
 const API_URI = "https://spark-mea.com";
 const currentURI = API_URI;
-console.log(currentURI);
 const FRONTEND_URI = 'http://localhost:3000'
 const instance = axios.create({
     baseURL: API_URI,
